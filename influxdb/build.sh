9c05a876be49de1955883b9d232c936b4b097df2
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2016-06-08 17:51:42 +0000

    Enable influxdb docker image build for multiple architectures

--- influxdb/build.sh
+++ influxdb/build.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+ARCH=$( uname -m )
 pushd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-docker build -t heapster_influxdb:canary .
+if [ ${ARCH} == "x86_64" ]; then
+	docker build -t heapster_influxdb:canary -f Dockerfile .
+elif [ ${ARCH} == "ppc64le" ]; then
+	docker build -t heapster_influxdb:canary -f Dockerfile.ppc64le .
+fi
 popd
